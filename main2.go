abd18f78343ffff557eb01337a78d50177dae503
lvxiaorun <173360368@qq.com>
2017-09-25 17:56:18 +0800

    part

--- main2.go
+++ main2.go
@@ -6,10 +6,12 @@ import (
 	"golang.org/x/net/websocket"
 	"net/http"
 	"html/template"
+	"encoding/json"
 )
 
 var allData AllData
 var allUser map[string]UserInfo
+
 func main() {
 	fmt.Println("启动时间")
 	fmt.Println(time.Now())
@@ -28,20 +30,52 @@ func main() {
 
 func h_index2(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
-	t,_ := template.ParseFiles("index.html")
-	t.Execute(w,name)
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, name)
 	//http.ServeFile(w, r, "index2.html")
 }
 
-func h_webSocket2(ws *websocket.Conn){
+func h_webSocket2(ws *websocket.Conn) {
 	var msg Message
 	var data string
+J:
 	for {
 		msglen := len(allData.Messages)
 		fmt.Println("Msgs", msglen, "allUser长度：", len(allUser))
+		//有消息时,判断发给个人还是群组发送
+		if msglen > 0 {
+			//b,err := json.Marshal(allData)
+			//if err != nil{
+			//	fmt.Println("全局信息异常:",err)
+			//	break
+			//}
+			for key, value := range allUser {
+				partdata := AllData{}
+				partdata.UserInfos = allData.UserInfos
+				for _, item := range allData.Messages {
+					if item.ToUser == key {
+						partdata.Messages = append(partdata.Messages, item)
+					}
+				}
+				b, errh := json.Marshal(partdata)
+				if errh != nil {
+					fmt.Println("序列化用户全局消息失败,user:", key, ":", value)
+					break J
+				}
+				errsend := websocket.Message.Send(value.Conn, string(b))
+				if errsend != nil{
+					fmt.Println("发送消息失败,user:",key,":",value)
+					delete(allUser,key)
+					break J
+				}
+			}
+			allData.UserInfos = make([]UserInfo,0)
+		}
+		fmt.Println("开始解析数据")
 	}
 
 }
+
 type Message struct {
 	UserName string
 	Msg      string
@@ -51,10 +85,10 @@ type Message struct {
 
 type UserInfo struct {
 	UserName string
-	Conn *websocket.Conn
+	Conn     *websocket.Conn
 }
 
 type AllData struct {
-	Messages []Message
+	Messages  []Message
 	UserInfos []UserInfo
 }
